226ca59723f7ed0ad03028ef34c7d05690685f47
Vinicius <yinioliveira@gmail.com>
2025-06-13 02:23:12 -0300

    adicionei as funções principais para o CLI de agendamento

---
+++ src/day.py
@@ -0,0 +1,14 @@
+from month import get_days_in_month
+
+def select_day(month, year):
+    max_days = get_days_in_month(month, year)
+
+    while True:
+        try:
+            day = int(input(f"\nSelecione um dia (1-{max_days}): "))
+            if day < 1 or day > max_days:
+                raise ValueError
+            return day
+        
+        except ValueError:
+            print("Dia inválido. Tente novamente.")
---
+++ src/main.py
@@ -0,0 +1,16 @@
+from year import select_year
+from month import select_month
+from day import select_day
+
+def main():
+    print("\n----- Agendar uma reunião ----- ")
+    print("\nSelecione a data que deseja marcar: ")
+
+    year = select_year()
+    month, month_name = select_month()
+    day = select_day(month, year)
+
+    print(f"\nReunião agendada para: {day} de {month_name} de {year}")
+
+if __name__ == "__main__":
+    main()
